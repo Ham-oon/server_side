58465cb55915eabe5199f8776eb385447fc768d5
Ham-oon <f14amir@gmail.com>
2023-10-22 18:20:25 +0330

    فایل مقایسه

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,6 +25,27 @@ public class HomeController : Controller
     public IActionResult ContactUs()
     {
         return View();
+
+        int a = 10;
+        string b ="in the name of god";
+        float c = 123.45f;
+        decimal d = 182.12312434m;
+        int e = default;
+        bool aa = default;
+
+        // ارور هندلینگ با try catch
+
+        try{
+
+            string bb = "123465758abbb";
+            int ab = int.Parse(bb);
+
+        }
+        catch(Exception ERR){
+
+            // return ERR.Message;
+        }
+
     }
     public IActionResult Privacy()
     {
